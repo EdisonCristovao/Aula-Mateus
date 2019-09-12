07975f8084a34455ff0fa4263da1776f2eaaa8e4
Edison Cristovao <edison.cr.jr@gmail.com>
2019-09-12 18:16:38 -0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n, i;
+  cout << "Digite o tamanho do Tab";
+  cin >> n;
+
+  int tab[n];
+
+  // iniciando tab com 0 ou 1
+  for (i = 0; i < n; i++)
+  {
+    cin >> tab[i];
+  }
+
+  //criar tab com numero de bombas
+  for (i = 0; i < n; i++)
+  {
+    if (i == 0)
+    {
+    }
+
+    if (i == n - 1)
+    {
+    }
+
+    else
+    {
+    }
+  }
+
+  //  for (i = 0; i < n; i++)
+  // {
+  //  cout << tab[i];
+  //}
+
+  return 0;
+}
